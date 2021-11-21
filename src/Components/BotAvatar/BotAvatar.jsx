e4cafa1febd54493383803d47f10bd2f0696cb66
RoyalCheungg <rcheun52@uwo.ca>
2021-11-20 22:00:33 -0500

    llama

---
+++ src/Components/BotAvatar/BotAvatar.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+import llama from "./cutellama-mod.jpg";
+
+
+const BotAvatar = () => {
+    return (
+        <div>
+            <img src={llama} alt="llama" />
+        </div>
+
+    )
+}
+
+export default BotAvatar;
