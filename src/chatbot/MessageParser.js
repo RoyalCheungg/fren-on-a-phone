d854f3951f2e6d0f41645593b3bc3afe1f7c78ff
Rithik <71098038+Aaron-Moses@users.noreply.github.com>
2021-11-21 01:07:22 -0500

    Added messages

--- src/chatbot/MessageParser.js
+++ src/chatbot/MessageParser.js
@@ -1,9 +1,17 @@
 import socketIOClient from 'socket.io-client';
 import { io } from "socket.io-client";
+
 var WebSocketClient = require('websocket').client;
 const MonkeyLearn = require('monkeylearn')
 // MessageParser starter code
+
 class MessageParser {
+  static messageOptions = [
+    ["Keep being Silly, Honest, Kind.", "Its a nice day today isnt it", "I like your attitude!", "Wowzers! You're a-ok. Don't stop believing", "Because im happy! Clap along if you feel like happiness is the truth ..."],
+    ["Today, Tomorrow, Yesterday ... It's all the same.", "Interesting! Tell me more...", "What are you passionate about?", "Sometimes I'm happy and I'm not a human", "Mary HAD a little lamb"],
+    ["Focus on the journey, not the destination.", "Well, thats too bad );","Cheer up! You got this!","I'm sorry you're having a bad day... but remember nothing is permanent", "Don't let others put you down. You are in charge of your life."]
+  ]
+
     constructor(actionProvider, state) {
       this.actionProvider = actionProvider;
       this.state = state;
